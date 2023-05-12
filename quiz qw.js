677761616c548fb2221167207f34b61c079dca52
MentosNoMint <113000714+MentosNoMint@users.noreply.github.com>
2023-05-12 11:27:30 +0500

    Add files via upload

--- quiz qw.js
+++ quiz qw.js
@@ -1,14 +1,17 @@
 let quizId = 1;
 const sumbit = document.getElementById('sumbit')
 const timer = document.getElementById('timer')
+let score = 0
+
+
 
 
 async function getResponce() {
-    let responce = await fetch(`http://localhost:3000/quiz/answer/${quizId}`, {
+
+    let responce = await fetch(`http://localhost:4000/quiz/answer/${quizId}`, {
         method: 'GET',
     })
     const content = await responce.json()
-
     let key;
 
     const list = document.querySelector('.text')
@@ -16,31 +19,39 @@ async function getResponce() {
     for (key in content)
         list.innerHTML = `
         <li class="answers">
-        <input name="answer" type="radio" id="a" class="answer">
-        <label for="a" id="answer_a">${content[0]['answer']}</label>
+        <input name="answer" type="radio" id="b" class="answer" onclick="setCurrAnswer(${content[0]['is_correct']})"
+        >
+        <label for="b" id="answer_b">${content[0]['answer']}</label>
     </li>
     <li class="answers">
-        <input name="answer" type="radio" id="b" class="answer">
+        <input name="answer" type="radio" id="b" class="answer" onclick="setCurrAnswer(${content[1]['is_correct']})"
+        >
         <label for="b" id="answer_b">${content[1]['answer']}</label>
     </li>
     <li class="answers">
-        <input name="answer" type="radio" id="c" class="answer">
-        <label for="c" id="answer_c">${content[2]['answer']}</label>
+        <input name="answer" type="radio" id="b" class="answer" onclick="setCurrAnswer(${content[2]['is_correct']})"
+        >
+        <label for="b" id="answer_b">${content[2]['answer']}</label>
     </li>
     <li class="answers">
-        <input name="answer" type="radio" id="d" class="answer">
-        <label for="d" id="answer_d">${content[3]['answer']}</label>
-    </
+        <input name="answer" type="radio" id="b" class="answer" onclick="setCurrAnswer(${content[3]['is_correct']})"
+        >
+        <label for="b" id="answer_b">${content[3]['answer']}</label>
+    </li>
   `
     console.log(content[key]);
+
+
 }
 
 
 getResponce();
 
 
+
+
 async function getResponceAns() {
-    let responceAnswer = await fetch(`http://localhost:3000/quiz/${quizId}`, {
+    let responceAnswer = await fetch(`http://localhost:4000/quiz/${quizId}`, {
         method: 'GET',
     })
     const contentAnswer = await responceAnswer.json();
@@ -53,46 +64,70 @@ async function getResponceAns() {
 `
 }
 
+
+
+
 getResponceAns()
 
 async function nextQuestions() {
     sumbit.addEventListener('click', function () {
         quizId++
 
+        setCurrAnswer()
         return getResponce(), getResponceAns()
+
     })
     getTime()
-    console.log()
+
 }
+
+
+const setCurrAnswer = (isTrue) => {
+    if (isTrue) {
+        score++
+        console.log("Правильно")
+    } else {
+        console.log("Неа")
+    }
+}
+
 
 nextQuestions()
 
 
-async function getTime() {
-    let count = 15
+// async function getTime() {
+//     let count = 15
 
-    countdown = setInterval(() => {
-        count--
-        timer.innerHTML = `${count}s`
-        if (count == 0) {
-            quizId++
-            
-            nextQuestions()
-            clearInterval(countdown)
-            return nextQuestions(), getResponce(), getResponceAns()
-        }
-    }, 1000)
-}
+//     countdown = setInterval(() => {
+//         count--
+//         timer.innerHTML = `${count}s`
+//         if (count == 0) {
+//             quizId++
 
-getTime()
+//             nextQuestions()
+//             clearInterval(countdown)
+//             return nextQuestions(), getResponce(), getResponceAns()
+//         }
+//     }, 1000)
+// }
 
 
-
+// getTime();
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+    
 
 
 
