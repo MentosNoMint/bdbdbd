b25e71830c979abb5222fd338c8a49d0aff1a5da
MentosNoMint <113000714+MentosNoMint@users.noreply.github.com>
2023-06-08 12:59:15 +0500

    Add files via upload

--- biblos.inc-project-main/FormQuiz.js
+++ biblos.inc-project-main/FormQuiz.js
@@ -4,18 +4,40 @@ form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   try {
+    let questions_id = 1
     // получаем данные из формы
     const formData = new FormData(form);
-    const name = formData.get('name');
-    const email = formData.get('email');
-    const message = formData.get('message');
-
-    // отправляем POST запрос на backend
-    const response = await fetch('http://localhost:4000/users', {
+    const name = formData.get('nameQuiz');
+    const questions = formData.get('questions');
+    const answer = formData.get('answer')
+   
+    console.log(answer)
+    await fetch('http://localhost:4000/answer/add', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, email, message }),
+      body: JSON.stringify({answer , questions_id}),
     });
+    // отправляем POST запрос на backend
+    const nameId = await fetch('http://localhost:4000/name/id', {
+      method: 'GET',
+    });
+    const resId = await nameId.json()
+
+    let resultNameId = resId.map(a => a.id)
+    let category_id = resultNameId[resultNameId.length - 1]
+
+    const response = await fetch('http://localhost:4000/name/quiz/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({name}),
+    });
+
+    await fetch('http://localhost:4000/question/quiz/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({questions, category_id}),
+    });
+
 
     // обрабатываем ответ от backend
     const data = await response.json();
